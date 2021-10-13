98627211288cba8a98d3d76bc06dbc74eb2b8266
Ryan <ryanrana_4@hotmail.com>
2021-10-13 03:04:19 +0100

    basic functionality very buggy

--- myscript.js
+++ myscript.js
@@ -1,3 +1,39 @@
+const display = document.querySelector("#lowerDisplay");
+const upperDisplay = document.querySelector("#upperDisplay");
+display.textContent = "";
+let operator = "";
+let first = "";
+let second = "";
+
+
+const buttons = (document.querySelectorAll(".button"));
+    buttons.forEach(button => 
+        button.addEventListener("click", updateDisplay));
+
+
+function updateDisplay(e) {
+    console.log(this.className);
+
+    if(this.id === "clear") {
+        display.textContent = "";
+        upperDisplay.textContent = "";
+    } else if (this.className === "button operator") {
+        first = display.textContent;
+        operator = this.id;
+        upperDisplay.textContent = display.textContent;
+        display.textContent ="";     
+    } else if(this.id === "=") {
+        second = display.textContent;
+        upperDisplay.textContent = "";
+        display.textContent = operate(operator, first, second);
+
+    }else
+
+
+    display.textContent = display.textContent += this.textContent;
+}
+
+
 function add(a, b) {
     return a + b;
 }
